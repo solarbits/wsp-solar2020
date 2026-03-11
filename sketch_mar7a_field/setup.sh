842a5d6c64e46df1dd9058788d552d8dd470d89a
Eric George <errkk@hey.com>
2026-03-11 21:40:38 +0000

    putting bin cos omg

--- sketch_mar7a_field/setup.sh
+++ sketch_mar7a_field/setup.sh
@@ -10,19 +10,19 @@ set -e
 BOARD_MANAGER_URL="https://apps.industrialshields.com/main/arduino/boards/package_industrialshields_index.json"
 
 echo "Adding Industrial Shields board manager URL..."
-arduino-cli config add board_manager.additional_urls "$BOARD_MANAGER_URL"
+bin/arduino-cli config add board_manager.additional_urls "$BOARD_MANAGER_URL"
 
 echo "Installing Industrial Shields AVR core 1.2.1..."
-arduino-cli core update-index
-arduino-cli core install industrialshields:avr@1.2.1
+bin/arduino-cli core update-index
+bin/arduino-cli core install industrialshields:avr@1.2.1
 
 echo "Installing libraries..."
-arduino-cli lib install ArduinoJson@5.13.5
-arduino-cli lib install PubSubClient@2.8
+bin/arduino-cli lib install ArduinoJson@5.13.5
+bin/arduino-cli lib install PubSubClient@2.8
 
 # Modbus library from Industrial Shields is not in the Arduino Library Manager,
 # so it is vendored in libraries/Modbus (from https://github.com/Industrial-Shields/arduino-Modbus)
 
 echo ""
 echo "Done. To compile:"
-echo "  arduino-cli compile --fqbn industrialshields:avr:mduino:cpu=mduino19rplus --library libraries/Modbus sketch_mar7a_field.ino"
+echo "  bin/arduino-cli compile --fqbn industrialshields:avr:mduino:cpu=mduino19rplus --library libraries/Modbus sketch_mar7a_field.ino"
